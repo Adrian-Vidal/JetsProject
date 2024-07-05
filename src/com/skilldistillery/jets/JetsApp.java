12e3d5bc70b4f36401442354631ebf58c1faf7f7
Adrian <adrian.vidal9589@gmail.com>
2024-07-04 22:29:29 -0600

    Making progress on the JetsAp menup.

--- src/com/skilldistillery/jets/JetsApp.java
+++ src/com/skilldistillery/jets/JetsApp.java
@@ -15,7 +15,33 @@ public class JetsApp {
 	}
 
 	public void launchApp() {
-
-	}
+		int userSelection;
+		System.out.println("Welcome to the Jets App. Press the enter/return button to start.");
+		keyboard.nextLine();
+		System.out.println("The airfield is currently empty. First, please select number 7 from the menu to add a jet to the fleet:\n");
+		do {
+		System.out.println("1.) List Fleet\n"
+				+ "2.) Fly all jets\n"
+				+ "3.) View fastest jet\n"
+				+ "4.) View jet with longest range\n"
+				+ "5.) Load all Cargo Jets\n"
+				+ "6.) Dogfight!"
+				+ "7.) Add a jet to the fleet\n"
+				+ "8.) Remove a jet from the fleet\n"
+				+ "9.) Quit");
+				userSelection = keyboard.nextInt();
+				
+				switch (userSelection) {
+				case 1:
+					AirField fleet = new AirField();
+					fleet.listJets();
+		
+				
+				}
+				} while (userSelection != 9);
+		
+		
+		
+		}
 
 }
